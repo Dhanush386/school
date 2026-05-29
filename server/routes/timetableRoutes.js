7a84d4ddbb4a88f940401f59ca9772ba5b4b9a54
Dhanush <Dhanush@example.com>
2026-05-29 17:31:40 +0530

    fix: correct middleware folder name in timetableRoutes

--- server/routes/timetableRoutes.js
+++ server/routes/timetableRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getTimetable, saveTimetable } = require('../controllers/timetableController');
-const { protect, restrictTo } = require('../middlewares/auth');
+const { protect, restrictTo } = require('../middleware/auth');
 
 router.use(protect);
 
